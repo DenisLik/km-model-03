d40f06bc82ae5662f665c29ee1a5a513547c9e42
Denis Lik <delick2@gmail.com>
2022-06-07 12:08:35 +0300

    second commit

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
-# загрузка библиотек
+# Importing Necessary modules
 import uvicorn
 from fastapi import FastAPI
-from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 import pickle as pkl
 import numpy as np
 
 
-# загрузка модели для предсказания
+# Loading predict model
 predict_model = pkl.load(open("model/saved_model_01.pkl","rb"))
 
 
-# создание экзампляра класса FastAPI
+# Declaring our FastAPI instance
 app_model_01 = FastAPI()
 
-# Создание класса для определения тела запроса и подсказок типа каждого атрибута
+# Creating class to define the request body
+# and the type hints of each attribute
 class request_body(BaseModel):
     user_id : int
     mean_score : float
 
 
 
-# # Создание конечной точки Endpoint для получения данных для прогнозирования
+# Creating an Endpoint to receive the data to make prediction on.
 @app_model_01.post('/predict')
 def predict(data: request_body):
-    # Приведение данных в форму, подходящую для прогнозирования
+    # Making the data in a form suitable for prediction
     test_data_a = [[
             data.user_id,
             data.mean_score
     ]]
     
-    #user_idx = test_data_a[0]
+    # user_idx = test_data_a[0]
     test_data_b = np.array(test_data_a).reshape(-1, 1)
-    # Предсказывание класса (1 - готов к сдаче, 0 - не готов)
+    # Predicting the Class
     class_idx = predict_model.predict(test_data_b)[1]
-    # Возврат результата предсказания
+    # Return the Result
     return {'user_id': data.user_id, 'prediction': class_idx}    
 
 
